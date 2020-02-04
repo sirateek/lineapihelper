40fa7249b69281eab9ca3f82a70fc6cb7b073d6b
Siratee K <sirateek@gmail.com>
2020-02-04 16:10:39 +0700

    Dialogflow Request support update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var channelaccessToken = "";
 var channelsecret = "";
 
 var req;
+var dialogflow = false;
 module.exports = {
   channelaccesstoken: function(cat) {
     if (typeof cat === "string") {
@@ -169,6 +170,9 @@ module.exports = {
     if (typeof requestdata !== "object") {
       throw new Error("The request must be a request object");
     }
+    if (requestdata.headers["user-agent"] === "Google-Dialogflow") {
+      dialogflow = true;
+    }
     req = requestdata;
     console.log(JSON.stringify(req.body));
     return true;
@@ -197,6 +201,9 @@ module.exports = {
         "You didn't set the channel secret properly, Please use .channelsecret('')"
       );
     }
+    if (dialogflow) {
+      return true;
+    }
     const crypto = require("crypto");
     var reqbody = JSON.stringify(req.body);
     const linesignature = crypto
@@ -216,7 +223,13 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    const userId = req.body.events[0].source.userId;
+    var userId;
+    if (dialogflow) {
+      userId = req.body.originalDetectIntentRequest.payload.data.source.userId;
+    } else {
+      userId = req.body.events[0].source.userId;
+    }
+
     if (typeof userId === "undefined") {
       return false;
     } else {
@@ -229,7 +242,13 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    const roomId = req.body.events[0].source.roomId;
+    var roomId;
+    if (dialogflow) {
+      roomId = req.body.originalDetectIntentRequest.payload.data.source.roomId;
+    } else {
+      roomId = req.body.events[0].source.roomId;
+    }
+
     if (typeof roomId === "undefined") {
       return false;
     } else {
@@ -242,7 +261,14 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    const groupId = req.body.events[0].source.groupId;
+    var groupId;
+    if (dialogflow) {
+      groupId =
+        req.body.originalDetectIntentRequest.payload.data.source.groupId;
+    } else {
+      groupId = req.body.events[0].source.groupId;
+    }
+
     if (typeof groupId === "undefined") {
       return false;
     } else {
@@ -255,7 +281,13 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    const sourcetype = req.body.events[0].source.type;
+    var sourcetype;
+    if (dialogflow) {
+      sourcetype =
+        req.body.originalDetectIntentRequest.payload.data.source.type;
+    } else {
+      sourcetype = req.body.events[0].source.type;
+    }
     if (typeof sourcetype === "undefined") {
       return false;
     } else {
@@ -268,7 +300,12 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    const eventtype = req.body.events[0].type;
+    var eventtype;
+    if (dialogflow) {
+      eventtype = req.body.originalDetectIntentRequest.payload.data.type;
+    } else {
+      eventtype = req.body.events[0].type;
+    }
     if (typeof eventtype === "undefined") {
       return false;
     } else {
@@ -281,7 +318,12 @@ module.exports = {
         "You didn't set the request in lineapihelper properly, Please use .setrequest('Request object') to set the request before using this command"
       );
     }
-    var messagebody = req.body.events[0].message;
+    var messagebody;
+    if (dialogflow) {
+      messagebody = req.body.originalDetectIntentRequest.payload.data.message;
+    } else {
+      messagebody = req.body.events[0].message;
+    }
     return messagebody;
   },
   getcontent: function(messageId) {
