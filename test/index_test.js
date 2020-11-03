81ecb507d9c6f3d1d805252329b2effc8c2bd272
sirateek <sirateek@gmail.com>
2020-11-04 01:11:03 +0700

    Update test credential

--- test/index_test.js
+++ test/index_test.js
@@ -1,10 +1,8 @@
 const lah = require("../lib/index");
 
-lah.init(
-  "mMs7vcuIp5/2CcBkPFt9xwRdxtvKIzauXAUqJWRVrjLZNIl07pRCGcjvxM6bMRnemrGZwgUZvqSnZdy9nXpHE+fFbYW9C0ltux+m/7cUHLxsWqxwKX22dW2R5f+9WGQMDRma2eBubr3MYVLYSJHmwwdB04t89/1O/w1cDnyilFU="
-);
+lah.init("ChannelAccessToken");
 lah.messageApi
-  .push("U1efbc797c7174dd636c047f5ca8eba42", [{ type: "text", text: "Hello!" }])
+  .push("U1234", [{ type: "text", text: "Hello!" }])
   .then((data) => {
     console.log(data);
   })
