46f85f8bb756876f9180d5a8b236c9fd73f3fd75
sirateek <sirateek@gmail.com>
2020-11-04 01:04:29 +0700

    Refactor and change entire package to use OOP style. Done messageApi>reply  and messageApi>push

--- test/index_test.js
+++ test/index_test.js
@@ -1,3 +1,14 @@
-const lah = require("..");
-var a = new lah.LineCredential("Test", "123");
-console.log(a.get_channel_access_token());
+const lah = require("../lib/index");
+
+lah.init(
+  "mMs7vcuIp5/2CcBkPFt9xwRdxtvKIzauXAUqJWRVrjLZNIl07pRCGcjvxM6bMRnemrGZwgUZvqSnZdy9nXpHE+fFbYW9C0ltux+m/7cUHLxsWqxwKX22dW2R5f+9WGQMDRma2eBubr3MYVLYSJHmwwdB04t89/1O/w1cDnyilFU="
+);
+lah.messageApi
+  .push("U1efbc797c7174dd636c047f5ca8eba42", [{ type: "text", text: "Hello!" }])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log("Error");
+    console.log(e);
+  });
