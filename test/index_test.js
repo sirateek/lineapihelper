9dca1524b2ba8eb83cf3590b73e8d416e3a8214c
sirateek <sirateek@gmail.com>
2020-11-04 12:21:49 +0700

    Add messageApi>multicast method

--- test/index_test.js
+++ test/index_test.js
@@ -1,12 +1,5 @@
 const lah = require("../lib/index");
 
-lah.init("ChannelAccessToken");
-lah.messageApi
-  .push("U1234", [{ type: "text", text: "Hello!" }])
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.log("Error");
-    console.log(e);
-  });
+lah.init("ChannelAccessToken", "ChannelSecret");
+
+lah.messageApi.reply("Test", [{ type: "text", text: "Hello" }]);
