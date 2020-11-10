a35a52d6fea5fd35e17733c61e30236ac9e4f9bd
sirateek <sirateek@gmail.com>
2020-11-10 09:46:44 +0700

    Add TypeChecker into the LineApiCredential

--- lib/LineApiCredential/LineApiCredential.ts
+++ lib/LineApiCredential/LineApiCredential.ts
@@ -8,10 +8,28 @@ export class LineApiCredential {
    * for the LINE APIs authorization.
    */
   constructor(CredentialConfig: LineApiCredentialConfig) {
+    this.isStringOrUndefind(
+      "ChannelAccessToken",
+      CredentialConfig.ChannelAccessToken
+    );
+    this.isStringOrUndefind("ChannelSecret", CredentialConfig.ChannelSecret);
     this.#ChannelAccessToken = CredentialConfig.ChannelAccessToken || null;
     this.#ChannelSecret = CredentialConfig.ChannelSecret || null;
   }
 
+  private isStringOrUndefind = (propertyName: string, value: any) => {
+    if (typeof value !== "string" && typeof value !== "undefined") {
+      throw new TypeError(
+        "The `" +
+          propertyName +
+          "` must be the string only (Recieved " +
+          typeof value +
+          "[" +
+          value +
+          "])"
+      );
+    }
+  };
   /**
    * IsHasChannelAccessToken is used to check if the `ChannelAccessToken` has the value (Not null)
    */
