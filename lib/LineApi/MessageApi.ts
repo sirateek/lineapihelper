be05af71208406d4dc5759a7f3a25eafb6ae9da9
sirateek <sirateek@gmail.com>
2020-12-17 16:39:50 +0700

    :New: Add LineApiCredential and the sendRequest Handler

--- lib/LineApi/MessageApi.ts
+++ lib/LineApi/MessageApi.ts
@@ -1,1 +1,15 @@
-export class MessageApi {}
+import { LineApiCredential } from "../LineApiCredential/LineApiCredential";
+import { LineApi } from "./LineApi";
+export class MessageApi extends LineApi {
+  constructor(ApiCredential: LineApiCredential | undefined) {
+    super(ApiCredential);
+  }
+
+  reply(replyToken: string, messages: Array<String>) {
+    this.sendRequest(
+      this.ApiPrefix.DEFAULT_API_PREFIX + "/reply",
+      { Authorization: "", "Content-Type": "" },
+      {}
+    );
+  }
+}
