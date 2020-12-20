f72adbbbb433298a497ffe8864bf4daa8dfc07ae
Siratee Kittiwitchaowakul <sirateek@Siratees-MacBook-Pro.local>
2020-12-21 01:27:49 +0700

    :Update: Done developing LineApiCore & add new method for push API

--- lib/LineApi/LineApi.ts
+++ lib/LineApi/LineApi.ts
@@ -1,37 +1,77 @@
-import { LineApiCredential } from "../LineApiCredential/LineApiCredential";
-import axios from "axios";
+import { LineApiCredential, RequestHeader } from "../LineApiCredential/LineApiCredential";
+import axios, { AxiosResponse } from "axios";
 
 export class LineApi {
-  #Credential: LineApiCredential | null = null;
-  ApiPrefix: ApiPrefixUrl = {
+  #Credential: LineApiCredential = new LineApiCredential({});
+  #ApiPrefix: ApiPrefixUrl = {
     DEFAULT_API_PREFIX: "api.line.me",
     DATA_API_PREFIX: "api-data.line.me",
   };
-  constructor(ApiCredential: LineApiCredential | undefined) {
-    this.#Credential = ApiCredential || null;
+
+  constructor(ApiCredential: LineApiCredential) {
+    this.#Credential = ApiCredential;
   }
 
-  sendRequest(url: string, header: HeaderObject, body: Object) {
-    if (this.#Credential == null) {
-      throw new Error(
-        "There is no API Credential. (You may need to call `.init()` to init API Credential)"
-      );
+  sendRequest(url: string,method: RequestMethod, header: RequestHeader | undefined, body: Object) {
+    let requestHeader: Object
+    try {
+      requestHeader = this.#Credential.generate_request_header()
+    }  catch(e) {
+      if (typeof header === "undefined") {
+        throw e
+      }
     }
-    return axios({
-      url: "https://" + url,
-      method: "POST",
-      headers: header,
-      data: JSON.stringify(body),
-    });
+    return new Promise<ApiResponse>(async (resolve, reject) => {
+      try{
+        const requestResult = await axios({
+          url: "https://" + url,
+          method: method,
+          headers: header || requestHeader,
+          data: JSON.stringify(body),
+        });
+        resolve(new ApiResponse(requestResult.status, requestResult.data, requestResult))
+      } catch(e) {
+        reject(new ApiResponse(e.response.status, e.response.data, e.response))
+      }
+    })
   }
+
+  get ApiPrefix() {
+    return this.#ApiPrefix
+  }
+}
+
+export class ApiResponse {
+  #statusCode: number | undefined
+  #responseBody: Object | undefined
+  #rawResponse: AxiosResponse | undefined
+
+  constructor(statusCode: number, responseBody: Object, rawResponse: AxiosResponse) {
+    this.#statusCode = statusCode
+    this.#responseBody = responseBody
+    this.#rawResponse = rawResponse
+  }
+
+  get statusCode() {
+    return this.#statusCode
+  }
+
+  get responseBody() {
+    return this.#responseBody
+  }
+
+  get rawResponse() {
+    return this.#rawResponse
+  }
+}
+
+export enum RequestMethod {
+  POST = "POST",
+  GET = "GET",
+  DELETE = "DELETE",
 }
 
 interface ApiPrefixUrl {
   DEFAULT_API_PREFIX: String;
   DATA_API_PREFIX: String;
-}
-
-export interface HeaderObject {
-  Authorization: String;
-  "Content-Type": String;
 }
--- lib/LineApi/MessageApi.ts
+++ lib/LineApi/MessageApi.ts
@@ -1,15 +1,32 @@
-import { LineApiCredential } from "../LineApiCredential/LineApiCredential";
-import { LineApi } from "./LineApi";
+import { LineApiCredential, RequestHeader } from "../LineApiCredential/LineApiCredential";
+import { LineApi, RequestMethod } from "./LineApi";
 export class MessageApi extends LineApi {
-  constructor(ApiCredential: LineApiCredential | undefined) {
+  constructor(ApiCredential: LineApiCredential) {
     super(ApiCredential);
   }
 
-  reply(replyToken: string, messages: Array<String>) {
-    this.sendRequest(
-      this.ApiPrefix.DEFAULT_API_PREFIX + "/reply",
-      { Authorization: "", "Content-Type": "" },
-      {}
+  reply(replyToken: string, messages: Array<Object>, Header: RequestHeader | undefined) {
+    return this.sendRequest(
+      this.ApiPrefix.DEFAULT_API_PREFIX + "/v2/bot/message/reply",
+      RequestMethod.POST,
+      Header,
+      {
+        "replyToken": replyToken,
+        "messages": messages
+      }
+    );
+  }
+
+  push(to: string, messages: Array<Object>, Header: RequestHeader | undefined) {
+    return this.sendRequest(
+      this.ApiPrefix.DEFAULT_API_PREFIX + "/v2/bot/message/push",
+      RequestMethod.POST,
+      Header,
+      {
+        "to": to,
+        "messages": messages
+      }
     );
   }
 }
+
