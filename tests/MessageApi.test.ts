9ec1990ed0cde9d8588fe0593c3f79eb87b585f6
Siratee K <sirateek@gmail.com>
2020-12-21 18:18:20 +0700

    :Test: Add test case for multicast and broadcast

--- tests/MessageApi.test.ts
+++ tests/MessageApi.test.ts
@@ -17,14 +17,43 @@ describe('MesageApiTest', function() {
         ChannelAccessToken: process.env.CHANNELACCESSTOKEN,
         ChannelSecret: process.env.CHANNELSECRET,
       })
-      const pushResult = await lah.MessageApi.push(process.env.USERID || "?", 
-      [
-        {
-          type: "text",
-          text: `Test From LAH 0.2.0 (${process.env.LAHRUNTESTFROM})`
-        }
-      ])
+      const pushResult = await lah.MessageApi.push(
+        process.env.USERID || "?", 
+        [
+          {
+            type: "text",
+            text: `Test Push From LAH 0.2.0 (${process.env.LAHRUNTESTFROM})`
+          }
+        ]
+      )
       expect(pushResult.statusCode).to.equal(200)
       expect(JSON.stringify(pushResult.responseBody)).to.equal('{}')
     }); 
+
+    it('test-multicast', async function() {
+      const multicastResult = await lah.MessageApi.multicast(
+        [process.env.USERID || "?"],
+        [
+          {
+            type: "text",
+            text: `Test Multicast From LAH 0.2.0 (${process.env.LAHRUNTESTFROM})`
+          }
+        ]
+      )
+      expect(multicastResult.statusCode).to.equal(200)
+      expect(JSON.stringify(multicastResult.responseBody)).to.equal('{}')
+    })
+
+    it('test-broadcast', async function() {
+      const broadcastResult = await lah.MessageApi.broadcast(
+        [
+          {
+            type: "text",
+            text: `Test Broadcast From LAH 0.2.0 (${process.env.LAHRUNTESTFROM})`
+          }
+        ]
+      )
+      expect(broadcastResult.statusCode).to.equal(200)
+      expect(JSON.stringify(broadcastResult.responseBody)).to.equal('{}')
+    })
 });
