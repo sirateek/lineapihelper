f39defda1e4bae93203a0e4c25d62b975af44f2b
Siratee K <sirateek@gmail.com>
2020-12-21 17:30:40 +0700

    :Update: Add to show the value if the exception has occurred in generate_request_header()

--- lib/LineApiCredential/LineApiCredential.ts
+++ lib/LineApiCredential/LineApiCredential.ts
@@ -8,32 +8,14 @@ export class LineApiCredential {
    * for the LINE APIs authorization.
    */
   constructor(CredentialConfig: LineApiCredentialConfig) {
-    this.#isStringOrUndefind(
-      "ChannelAccessToken",
-      CredentialConfig.ChannelAccessToken
-    );
-    this.#isStringOrUndefind("ChannelSecret", CredentialConfig.ChannelSecret);
     this.#ChannelAccessToken = CredentialConfig.ChannelAccessToken;
     this.#ChannelSecret = CredentialConfig.ChannelSecret;
   }
 
-  #isStringOrUndefind = (propertyName: string, value: any) => {
-    if (typeof value !== "string" && typeof value !== "undefined") {
-      throw new TypeError(
-        "The `" +
-          propertyName +
-          "` must be the string only (Recieved " +
-          typeof value +
-          "[" +
-          value +
-          "])"
-      );
-    }
-  };
 
   public generate_request_header(): RequestHeader {
     if (typeof this.#ChannelAccessToken !== "string") {
-      throw new Error(`Invalid API Credential. (You may need to call '.init()' to init API Credential or the credential is incorrectly passed) (Got: ChannelAccessToken: ${typeof this.#ChannelAccessToken})`)
+      throw new Error(`Invalid API Credential. (You may need to call '.init()' to init API Credential or the credential is incorrectly passed) (Got: ChannelAccessToken: ${this.#ChannelAccessToken}[${typeof this.#ChannelAccessToken}])`)
     }
     return {
       Authorization: "Bearer " + this.#ChannelAccessToken,
