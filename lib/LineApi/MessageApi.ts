09c1814e4f3df0c4c36081df1a6af70fe0670086
Siratee K <sirateek@gmail.com>
2020-12-21 17:35:57 +0700

    :Update: Add dogstring to reply and push method

--- lib/LineApi/MessageApi.ts
+++ lib/LineApi/MessageApi.ts
@@ -5,6 +5,13 @@ export class MessageApi extends LineApi {
     super(ApiCredential);
   }
 
+  /**
+   * 
+   * @param replyToken The replyToken from incomming request
+   * @param messages The list of the message object. More infomation https://developers.line.biz/en/docs/messaging-api/message-types/
+   * @param Header Optional. In case that you want to specified your own request header
+   * Call reply API
+   */
   reply(replyToken: string, messages: Array<Object>, Header?: RequestHeader | undefined) {
     return this.sendRequest(
       this.ApiPrefix.DEFAULT_API_PREFIX + "/v2/bot/message/reply",
@@ -17,6 +24,13 @@ export class MessageApi extends LineApi {
     );
   }
 
+  /**
+   * 
+   * @param to The target userId that you want to send the messages to
+   * @param messages The list of the message object. More infomation https://developers.line.biz/en/docs/messaging-api/message-types/
+   * @param Header Optional. In case that you want to specified your own request header
+   * Call push API
+   */
   push(to: string, messages: Array<Object>, Header?: RequestHeader | undefined) {
     return this.sendRequest(
       this.ApiPrefix.DEFAULT_API_PREFIX + "/v2/bot/message/push",
@@ -28,5 +42,7 @@ export class MessageApi extends LineApi {
       }
     );
   }
+
+
 }
 
