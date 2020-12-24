38f7af9d64dc6c8ed14f15fc7e58a0b0d2f97799
Siratee K <sirateek@gmail.com>
2020-12-24 15:26:57 +0700

    :New: Add support for get and delete richmenu API

--- lib/LineApi/RichmenuApi/RichmenuApi.ts
+++ lib/LineApi/RichmenuApi/RichmenuApi.ts
@@ -13,7 +13,7 @@ export class RichmenuApi extends LineApi {
    *
    * @param richMenuConfig The Richmenu Config Object Payload. Can be generated from `LINE Bot Designer`. More info https://developers.line.biz/en/services/bot-designer/
    * @param Header Optional. In case that you want to specified your own request header
-   * Call create new richmenu API
+   * Call create new Richmenu API
    * More info https://developers.line.biz/en/reference/messaging-api/#create-rich-menu
    */
   createRichmenu(richMenuConfig: Object, Header?: RequestHeader | undefined) {
@@ -22,6 +22,50 @@ export class RichmenuApi extends LineApi {
       method: RequestMethod.POST,
       header: Header,
       body: richMenuConfig,
+    });
+  }
+
+  /**
+   *
+   * @param Header Optional. In case that you want to specified your own request header
+   * Call Get Richmenu List API
+   * More info https://developers.line.biz/en/reference/messaging-api/#get-rich-menu-list
+   */
+  getRichmenuList(Header?: RequestHeader | undefined) {
+    return this.sendRequest({
+      uri: this.ApiPrefix.DEFAULT_API_PREFIX + "/v2/bot/richmenu/list",
+      method: RequestMethod.GET,
+      header: Header,
+    });
+  }
+
+  /**
+   *
+   * @param richmenuId The ID of Richmenu you want to get info.
+   * @param Header Optional. In case that you want to specified your own request header
+   * Call Get Richmenu API
+   * More info https://developers.line.biz/en/reference/messaging-api/#get-rich-menu
+   */
+  getRichmenu(richmenuId: string, Header?: RequestHeader | undefined) {
+    return this.sendRequest({
+      uri: this.ApiPrefix.DEFAULT_API_PREFIX + `/v2/bot/richmenu/${richmenuId}`,
+      method: RequestMethod.GET,
+      header: Header,
+    });
+  }
+
+  /**
+   *
+   * @param richmenuId The ID of Richmenu you want to delete
+   * @param Header Optional. In case that you want to specified your own request header
+   * Call Delete Richmenu API
+   * More info https://developers.line.biz/en/reference/messaging-api/#delete-rich-menu
+   */
+  deleteRichmenu(richmenuId: string, Header?: RequestHeader | undefined) {
+    return this.sendRequest({
+      uri: this.ApiPrefix.DEFAULT_API_PREFIX + `/v2/bot/richmenu/${richmenuId}`,
+      method: RequestMethod.DELETE,
+      header: Header,
     });
   }
 }
